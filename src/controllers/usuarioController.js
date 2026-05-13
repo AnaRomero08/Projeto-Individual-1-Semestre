b1ff595155ba90e874a3bb1c33aee742cb62a137
AnaRomero08 <ana.romero@sptech.school>
2026-05-12 23:14:06 -0300

    Atualização do projeto, com incerção no banco de dados

--- src/controllers/usuarioController.js
+++ src/controllers/usuarioController.js
@@ -1,5 +1,4 @@
 var usuarioModel = require("../models/usuarioModel");
-var aquarioModel = require("../models/aquarioModel");
 
 function autenticar(req, res) {
     var email = req.body.emailServer;
@@ -18,22 +17,14 @@ function autenticar(req, res) {
                     console.log(`Resultados: ${JSON.stringify(resultadoAutenticar)}`); // transforma JSON em String
 
                     if (resultadoAutenticar.length == 1) {
-                        console.log(resultadoAutenticar);
-
-                        aquarioModel.buscarAquariosPorEmpresa(resultadoAutenticar[0].empresaId)
-                            .then((resultadoAquarios) => {
-                                if (resultadoAquarios.length > 0) {
-                                    res.json({
-                                        id: resultadoAutenticar[0].id,
-                                        email: resultadoAutenticar[0].email,
-                                        nome: resultadoAutenticar[0].nome,
-                                        senha: resultadoAutenticar[0].senha,
-                                        aquarios: resultadoAquarios
-                                    });
-                                } else {
-                                    res.status(204).json({ aquarios: [] });
-                                }
-                            })
+                        // Aqui retornamos os dados do seu usuário (id_usuario, nome, email, senha, tem_pet)
+                        res.json({
+                            id: resultadoAutenticar[0].id_usuario,
+                            email: resultadoAutenticar[0].email,
+                            nome: resultadoAutenticar[0].nome,
+                            senha: resultadoAutenticar[0].senha,
+                            tem_pet: resultadoAutenticar[0].tem_pet
+                        });
                     } else if (resultadoAutenticar.length == 0) {
                         res.status(403).send("Email e/ou senha inválido(s)");
                     } else {
@@ -43,45 +34,58 @@ function autenticar(req, res) {
             ).catch(
                 function (erro) {
                     console.log(erro);
-                    console.log("\nHouve um erro ao realizar o login! Erro: ", erro.sqlMessage);
                     res.status(500).json(erro.sqlMessage);
                 }
             );
     }
-
 }
 
 function cadastrar(req, res) {
-    // Crie uma variável que vá recuperar os valores do arquivo cadastro.html
+    // Dados do Usuário
     var nome = req.body.nomeServer;
     var email = req.body.emailServer;
     var senha = req.body.senhaServer;
-    var fkEmpresa = req.body.idEmpresaVincularServer;
+    var telefone = req.body.telefoneServer;
+    var temPet = req.body.temPetServer;
 
-    // Faça as validações dos valores
+    // Dados do Pet
+    var nomePet = req.body.nomePetServer;
+    var idadePet = req.body.idadePetServer;
+    var pesoPet = req.body.pesoPetServer;
+    var diasCompanheirismo = req.body.diasServer;
+
+
+    if (pesoPet == "" || pesoPet == undefined) pesoPet = 0;
+    if (diasCompanheirismo == "" || diasCompanheirismo == undefined) diasCompanheirismo = 0;
     if (nome == undefined) {
         res.status(400).send("Seu nome está undefined!");
     } else if (email == undefined) {
         res.status(400).send("Seu email está undefined!");
     } else if (senha == undefined) {
         res.status(400).send("Sua senha está undefined!");
-    } else if (fkEmpresa == undefined) {
-        res.status(400).send("Sua empresa a vincular está undefined!");
     } else {
-
-        // Passe os valores como parâmetro e vá para o arquivo usuarioModel.js
-        usuarioModel.cadastrar(nome, email, senha, fkEmpresa)
+        // 1. Cadastra o Usuário primeiro
+        usuarioModel.cadastrar(nome, email, senha, telefone, temPet)
             .then(
                 function (resultado) {
-                    res.json(resultado);
+                    // Se o usuário marcou que tem pet (1), cadastramos o pet usando o ID recém gerado
+                    if (temPet == 1) {
+                        var idUsuarioGerado = resultado.insertId;
+
+                        usuarioModel.cadastrarPet(idUsuarioGerado, nomePet, idadePet, pesoPet, diasCompanheirismo)
+                            .then(function () {
+                                res.json(resultado);
+                            }).catch(function (erroPet) {
+                                console.log("Erro ao cadastrar pet:", erroPet);
+                                res.status(500).json(erroPet.sqlMessage);
+                            });
+                    } else {
+                        res.json(resultado);
+                    }
                 }
             ).catch(
                 function (erro) {
                     console.log(erro);
-                    console.log(
-                        "\nHouve um erro ao realizar o cadastro! Erro: ",
-                        erro.sqlMessage
-                    );
                     res.status(500).json(erro.sqlMessage);
                 }
             );
